319ff67a3e878537bd5d4870f366296c890636bb
Dominic Orchard <dom.orchard@gmail.com>
2017-09-11 22:07:43 +0100

    comment notes on desugaring

--- src/Syntax/Desugar.hs
+++ src/Syntax/Desugar.hs
@@ -7,6 +7,19 @@ import Checker.Coeffects (kindOfFromScheme)
 import Control.Monad.State.Strict
 import qualified System.IO.Unsafe as Unsafe (unsafePerformIO)
 
+-- 'desugar' erases pattern matches in function definitions
+-- with coeffect binders.
+-- e.g., for a definition 'd' in code as:
+--       f :: |Int| -> ...
+--       f |x| = e
+--
+--  then desguar d produces
+--       f :: |Int| -> ....
+--       f xFresh = let |x : Int| Nat = xFresh in e
+--
+-- Note that the explicit typing from the type signature is pushed
+-- inside of the definition to give an explicit typing on the coeffect-let
+-- binding.
 desugar :: Def -> Def
 desugar (Def var expr pats tys@(Forall ckinds ty)) =
   Def var (evalState (desguarPats expr pats ty []) (0 :: Int)) [] tys
