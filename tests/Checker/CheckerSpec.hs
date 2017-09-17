35c880b23b747133210d44234b3f897ec3f0d58f
Vilem <vl81@kent.ac.uk>
2017-09-17 03:57:49 +0100

    Allow examples to be put into subdirectories

--- tests/Checker/CheckerSpec.hs
+++ tests/Checker/CheckerSpec.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE DoAndIfThenElse #-}
+
 module Checker.CheckerSpec where
 
-import Control.Monad (forM_)
-import Data.List ((\\))
-import System.Directory
+import Control.Monad (forM, forM_)
+import Data.List ((\\), isSuffixOf)
+import System.Directory (doesDirectoryExist, listDirectory, makeAbsolute, withCurrentDirectory)
+import System.FilePath (splitDirectories)
 
 import Test.Hspec
 
@@ -10,17 +13,34 @@ import Checker.Checker
 import Syntax.Parser
 
 
+pathToExamples :: FilePath
+pathToExamples = "examples"
+
+brokenExamples :: FilePath -- these don't get run
+brokenExamples = "broken"
+
+fileExtension :: String
+fileExtension = ".gr"
+
 spec :: Spec
 spec = do
-  exampleFiles <- runIO $ listDirectory dir
-  let files = exampleFiles \\ exclude
-  forM_ files $ \file -> do
-    src <- runIO $ withCurrentDirectory dir $ readFile file
+  exFiles <- runIO $ exampleFiles pathToExamples
+  forM_ exFiles $ \file -> do
+    src <- runIO $ readFile file
     let (ast, nameMap) = parseDefs src
     checked <- runIO $ check ast False nameMap
-    describe file $
+    describe (short file) $
       it "typechecks" $
         checked `shouldBe` Right True
   where
-    dir = "examples"
-    exclude = ["rec.gr","gex0.gr"]
+    short = last . splitDirectories -- remove everything but the filename
+
+exampleFiles :: FilePath -> IO [FilePath]
+exampleFiles path = do
+  files <- listDirectory path
+  files' <- withCurrentDirectory path $ forM files $ \f -> do
+    f' <- makeAbsolute f
+    isDir <- doesDirectoryExist f'
+    if isDir && not (brokenExamples `isSuffixOf` f') then exampleFiles f'
+    else return [f' | fileExtension `isSuffixOf` f']
+  return $ concat files'
