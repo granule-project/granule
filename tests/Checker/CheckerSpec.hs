685439012769240990a8d20bd649c4201d089c93
dominic <dom.orchard@gmail.com>
2017-10-19 14:46:49 +0100

    test against ill typed files

--- tests/Checker/CheckerSpec.hs
+++ tests/Checker/CheckerSpec.hs
@@ -14,6 +14,9 @@ import Syntax.Parser
 pathToExamples :: FilePath
 pathToExamples = "examples"
 
+pathToNonExamples :: FilePath
+pathToNonExamples = "examples/illtyped"
+
  -- files in these directories don't get checked
 exclude1 :: FilePath
 exclude1 = "broken"
@@ -37,6 +40,22 @@ spec = do
             case result of
                 Left ex -> expectationFailure (show ex) -- an exception was thrown
                 Right checked -> checked `shouldBe` Right True
+
+    srcFiles' <- runIO illTypedFiles
+    forM_ srcFiles' $ \file ->
+      describe file $ it "does not typecheck" $ do
+        parsed <- try $ readFile file >>= parseDefs :: IO (Either SomeException _)
+        case parsed of
+          Left ex -> expectationFailure (show ex) -- parse error
+          Right (ast, nameMap) -> do
+            result <- try (check ast False nameMap) :: IO (Either SomeException _)
+            case result of
+                Left ex -> expectationFailure (show ex) -- an exception was thrown
+                Right checked -> checked `shouldBe` (Left "")
+
   where
     exampleFiles =
       find (fileName /=? exclude1 &&? fileName /=? exclude2) (extension ==? fileExtension) pathToExamples
+
+    illTypedFiles =
+      find always (extension ==? fileExtension) pathToNonExamples
