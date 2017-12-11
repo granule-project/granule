a02adb48110a54f818b27318d08ea1920303ac1f
Vilem <vl81@kent.ac.uk>
2017-12-11 22:52:14 +0000

    Change Nat to Nat= in List and N

--- source/Checker/Primitives.hs
+++ source/Checker/Primitives.hs
@@ -9,8 +9,8 @@ typeLevelConstructors =
   [ ("Int",  KType)
   , ("Float", KType)
   , ("Bool", KType)
-  , ("List", KFun (KConstr "Nat") (KFun KType KType))
-  , ("N", KFun (KConstr "Nat") KType)
+  , ("List", KFun (KConstr "Nat=") (KFun KType KType))
+  , ("N", KFun (KConstr "Nat=") KType)
   , ("One", KCoeffect)   -- Singleton coeffect
   , ("Nat",  KCoeffect)
   , ("Nat=", KCoeffect)
