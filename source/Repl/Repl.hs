4d4b0e07456f606c57f41c060845bc4f60137ee7
Preston Keel <pkeel85@gmail.com>
2018-03-01 11:02:13 -0500

    Additional error handling

--- source/Repl/Repl.hs
+++ source/Repl/Repl.hs
@@ -8,13 +8,13 @@
 
 module Repl.Repl where
 
--- import Control.Exception (SomeException, try)
+import Control.Exception (SomeException, try)
 import Control.Monad.State
 import System.Console.Haskeline
 -- import System.Console.Haskeline.MonadException
 import System.Exit
 import System.FilePath.Glob (glob)
-import Utils 
+import Utils
 import Repl.Queue
 import Syntax.Pretty
 import Syntax.Expr
@@ -28,7 +28,7 @@ type REPLStateIO = StateT (Queue (QDefName, QDefDef)) IO
 instance MonadException m => MonadException (StateT (Queue (QDefName, QDefDef)) m) where
     controlIO f = StateT $ \s -> controlIO $ \(RunIO run) -> let
                     run' = RunIO (fmap (StateT . const) . run . flip runStateT s)
-                    in fmap (flip runStateT s) $ f run'                       
+                    in fmap (flip runStateT s) $ f run'
 
 data QDefName = RVar Id  | DefName Id
     deriving (Show, Eq)
@@ -47,25 +47,25 @@ getQDef e@(x,y) = error $ "Failed to get definition from context. Mismatched var
 -- Extract only closed terms from queue
 getQCTM :: Queue (QDefName,QDefDef) -> Queue Qelm -> REPLStateIO (Queue Qelm)
 getQCTM (Queue [] []) qCV = return $ qCV
-getQCTM q qCV = getQDefM (headQ q) >>= (\x -> case x of 
+getQCTM q qCV = getQDefM (headQ q) >>= (\x -> case x of
                  cv -> getQCTM (tailQ q) (enqueue cv qCV))
-                 
+
 
 -- Extract only closed terms from queue, non-monadic version
 getQCT :: Queue (QDefName,QDefDef) -> Queue Qelm -> (Queue Qelm)
 getQCT (Queue [] []) qCV = qCV
-getQCT q qCV = case getQDef (headQ q) of 
+getQCT q qCV = case getQDef (headQ q) of
                  cv -> getQCT (tailQ q) (enqueue cv qCV)
-                 
-                
+
+
 io :: IO a -> REPLStateIO a
 io i = liftIO i
 
 prettyDef :: (QDefName, QDefDef) -> String
 prettyDef elem = case getQDef elem of
-    (a, t@(Def _ _ _ _ ty)) -> (pretty a)++" : "++(pretty ty)
+    (a, t@(Def _ _ _ _ ty)) -> (pretty a)++" : "++(show t)
 
-    
+
 pop :: REPLStateIO (QDefName, QDefDef)
 pop = get >>= return.headQ
 
@@ -74,7 +74,7 @@ push t = do
   q <- get
   put (q `snoc` t)
 
--- **** 
+-- ****
 -- unfoldQueue :: (Queue Qelm) -> (Queue Qelm)
 -- unfoldQueue q = fixQ q emptyQ step
  -- where
@@ -82,8 +82,8 @@ push t = do
    -- step e@(x,t) _ r = (mapQ (substDef t x) r) `snoc` e
     -- where
       -- substDef :: Def -> Def -> Qelm -> Qelm
-      -- substDef x t (y, t') = (y, subst x t t')  
-      
+      -- substDef x t (y, t') = (y, subst x t t')
+
 -- unfoldDefsInTerm :: (Queue Qelm) -> Expr -> Expr
 -- unfoldDefsInTerm q t =
    -- let uq = toListQ $ unfoldQueue q
@@ -92,88 +92,90 @@ push t = do
 containsTerm_Qelm :: Queue Qelm -> QDefName -> Bool
 containsTerm_Qelm (Queue f r) v@(RVar vnm) = ((foldl (\b (defName, defTerm)-> b || (vnm == defName)) False r) || (foldl (\b (defName, defTerm)-> b || (vnm == defName)) False  f ))
 containsTerm_Qelm (Queue f r) v@(DefName vnm) = ((foldl (\b (defName, defTerm)-> b || (vnm == defName)) False r) || (foldl (\b (defName, defTerm)-> b || (vnm == defName)) False  f ))
-      
+
 containsTerm :: Queue (QDefName,QDefDef) -> QDefName -> Bool
 containsTerm (Queue [] []) _ = False
-containsTerm q v = (containsTerm_Qelm (getQCT q emptyQ) v) 
+containsTerm q v = (containsTerm_Qelm (getQCT q emptyQ) v)
 
 processFilesREPL :: [FilePath] -> (FilePath -> REPLStateIO a) -> (FilePath -> REPLStateIO a) -> REPLStateIO [[a]]
 processFilesREPL globPatterns e f = forM globPatterns $ (\p -> do
     filePaths <- io $ glob p
-    case filePaths of 
+    case filePaths of
         [] -> (e p) >>= (return.(:[]))
         _ -> forM filePaths f)
 
 readToQueue :: (?globals::Globals) => FilePath -> REPLStateIO ExitCode
 readToQueue pth = do
-    pf <- io $ parseDefs =<< readFile pth    
+    pf <- io $ try $ parseDefs =<< readFile pth
     case pf of
-        (ast, maxFreshId) -> do
+      Right (ast, maxFreshId) -> do
             let ?globals = ?globals { freshIdCounter = maxFreshId }
             checked <- io $ check ast
             case checked of
-                Ok -> do                    
-                    forM ast $ \idef -> loadInQueue idef                               
+                Ok -> do
+                    forM ast $ \idef -> loadInQueue idef
                     return ExitSuccess
                 Failed -> return (ExitFailure 1)
-
-loadInQueue :: Def -> REPLStateIO ()       
+      Left (e :: SomeException) -> do
+            return $ print $ show e
+            return (ExitFailure 1)
+loadInQueue :: Def -> REPLStateIO ()
 loadInQueue def@(Def _ id _ _ _) = do
-  push (RVar id, DefTerm def) 
+  push (RVar id, DefTerm def)
 loadInQueue adt@(ADT _ _ _ _) = do
         return ()
-                
+
 noFileAtPath :: FilePath -> REPLStateIO ExitCode
 noFileAtPath pt = do
     io $ print $ "The file path "++pt++" does not exist"
     return (ExitFailure 1)
-                   
 
 
 
-                    
+
+
 handleCMD :: (?globals::Globals) => String -> REPLStateIO ()
 handleCMD "" = return ()
-handleCMD s =    
+handleCMD s =
    case (parseLine s) of
     Right l -> handleLine l
     Left msg -> io $ putStrLn msg
-  where    
+  where
     handleLine :: (?globals::Globals) => REPLExpr -> REPLStateIO ()
     handleLine DumpState = get >>= io.print.(mapQ prettyDef)
-        
-    
-    handleLine (LoadFile ptr) = do 
-      ecs <- processFilesREPL ptr noFileAtPath (let ?globals = ?globals in readToQueue)
-      if all (== ExitSuccess) (concat ecs) then io.putStrLn $ "File(s) loaded" else io.putStrLn $ "Error while loading file(s)" 
-        
-        
-                
-        
 
-   
+
+    handleLine (LoadFile ptr) = do
+      ecs <- processFilesREPL ptr noFileAtPath (let ?globals = ?globals in readToQueue)
+      if all (== ExitSuccess) (concat ecs) then io.putStrLn $ "File(s) loaded" else io.putStrLn $ "Error while loading file(s)"
+
+
+
+
+
+
 -- getFV :: Expr -> [Id Expr]
 -- getFV t = fv t :: [Id Expr]
 
-helpMenu :: String                          
-helpMenu = 
+helpMenu :: String
+helpMenu =
       "-----------------------------------------------------------------------------------\n"++
       "                  The Granule Help Menu                                         \n"++
       "-----------------------------------------------------------------------------------\n"++
       ":help             (:h)  Display the help menu\n"++
       ":quit             (:q)  Quit Granule\n"++
       ":show <term>      (:s)  Display the Abstract Syntax Type of a term\n"++
-      ":unfold <term>    (:u)  Unfold the expression into one without toplevel definition.\n"++ 
+      ":unfold <term>    (:u)  Unfold the expression into one without toplevel definition.\n"++
       ":dump             (:d)  Display the context\n"++
       ":load <filepath>  (:l)  Load an external file into the context\n"++
       "-----------------------------------------------------------------------------------"
-          
+
 repl :: IO ()
 repl = do
   evalStateT (runInputT defaultSettings loop) emptyQ
-   where 
+   where
        loop :: InputT REPLStateIO ()
-       loop = do           
+       loop = do
            minput <- getInputLine "Granule> "
            case minput of
                Nothing -> return ()
@@ -181,6 +183,6 @@ repl = do
                Just input | input == ":q" || input == ":quit"
                               -> liftIO $ putStrLn "Leaving Granule." >> return ()
                           | input == ":h" || input == ":help"
-                              -> (liftIO $ putStrLn helpMenu) >> loop                                 
-                          | otherwise -> let ?globals = defaultGlobals 
+                              -> (liftIO $ putStrLn helpMenu) >> loop
+                          | otherwise -> let ?globals = defaultGlobals
                                           in (lift.handleCMD $ input) >> loop
