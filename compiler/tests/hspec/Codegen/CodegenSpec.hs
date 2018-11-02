21895ff946b40b1276fa2f433b930d304a48c697
Edward J Brown <edward.brown.96@live.co.uk>
2018-11-02 20:37:33 +0000

    Added compiler dummy spec tests.
    - Changed the names of the spec/doctest components to avoid the
      ambiguity error when using component syntax with stack test.

---
+++ compiler/tests/hspec/Codegen/CodegenSpec.hs
@@ -0,0 +1,13 @@
+module Codegen.CodegenSpec where
+
+import Test.Hspec hiding (Spec)
+import qualified Test.Hspec as Test
+import Test.QuickCheck
+import Codegen.Codegen
+
+spec :: Test.Spec
+spec = do
+  describe "Compiler Test"
+    $ it "" $ True `shouldBe` True
+--    $ it "is not commutative"
+--    $ property (\e1 e2 -> intersectCtxts e1 e2 /= intersectCtxts e2 e1)
