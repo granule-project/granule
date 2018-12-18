c53b3e90f9f7cbbe38ad0efa5e46a9dd4907e1d7
Edward J Brown <edward.brown.96@live.co.uk>
2018-12-18 12:31:09 +0000

    Added Data.Bifunctor.Foldable module.
    This adds a version of cata which works for mutually recursive
    bifunctors `bicata`.

---
+++ frontend/tests/hspec/Data/Bifunctor/FoldableSpec.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.Bifunctor.FoldableSpec where
+
+import Test.Hspec hiding (Spec)
+import qualified Test.Hspec as Test
+import Data.Bifunctor.Foldable
+import Data.Bifunctor.TH
+
+data ExprF expr val =
+    AndF expr expr
+    | OrF expr expr
+    | NotF expr
+    | ValF val
+$(deriveBifunctor ''ExprF)
+
+type Expr = Fix2 ExprF ValueF
+
+pattern And l r = (Fix2 (AndF l r))
+pattern Or l r = (Fix2 (OrF l r))
+pattern Not v = (Fix2 (NotF v))
+pattern Val v = (Fix2 (ValF v))
+
+data ValueF val expr =
+    LitF Bool
+    | IgnoreF expr Bool -- `expr` does nothing, it's just here to
+                        -- demonstrate mutual recursion.
+$(deriveBifunctor ''ValueF)
+
+type Value = Fix2 ValueF ExprF
+
+pattern Lit b = (Fix2 (LitF b))
+pattern Ignore e b = (Fix2 (IgnoreF e b))
+
+spec :: Test.Spec
+spec = do
+  describe "Mutually Recursive Bifunctors" $
+    it "cata works for bools" $
+      let expr = (And
+                    (Or
+                        (Val (Lit True))
+                        (Val (Lit False)))
+                    (Val
+                        (Ignore
+                            (Not
+                                (Val (Lit True))) False))) :: Expr
+
+      in bicata evalExpr evalVal expr `shouldBe` False
+         where evalExpr (AndF l r) = l && r
+               evalExpr (OrF l r) = l || r
+               evalExpr (NotF v) = not v
+               evalExpr (ValF v) = v
+               evalExpr :: ExprF Bool Bool -> Bool
+
+               evalVal (LitF v) = v
+               evalVal (IgnoreF b r) = r
+               evalVal :: ValueF Bool Bool -> Bool
