f71193651569de9cae33c46bd524d1f6908f0a05
Edward J Brown <edward.brown.96@live.co.uk>
2018-12-29 19:14:23 +0000

    Added Topsort pass to compiler.
    - This pass sorts the value definitions into the order their initialisers should run, rejecting asts where no such order is safe.

---
+++ compiler/src/Language/Granule/Codegen/TopsortDefinitions.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Language.Granule.Codegen.TopsortDefinitions where
+
+import Language.Granule.Syntax.Def
+import Language.Granule.Syntax.Expr
+import Language.Granule.Syntax.Identifiers
+import Language.Granule.Codegen.NormalisedDef
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Either
+import Data.Bifunctor.Foldable
+import Data.Bifoldable
+
+import Data.Graph
+import Data.Tree
+
+data TopsortResult ev a =
+    Ok (NormalisedAST ev a)
+    | RecursiveValues [ValueDef ev a]
+    | InitializationCycle [FunctionDef ev a] [ValueDef ev a]
+
+deriving instance (Show ev, Show a) => Show (TopsortResult ev a)
+deriving instance (Eq ev, Eq a) => Eq (TopsortResult ev a)
+
+topologicalSortDefinitions :: (Show ev, Show a) =>  NormalisedAST ev a -> TopsortResult ev a
+topologicalSortDefinitions (NormalisedAST dataDefs functionDefs valueDefs)
+    | any isRecursiveValue valueDefs =
+        RecursiveValues $ filter isRecursiveValue valueDefs
+    | otherwise =
+        case topSortValueDefinitions functionDefs valueDefs of
+            Right (cyclicalFunctionDefs, cyclicalValueDefs) ->
+                InitializationCycle cyclicalFunctionDefs cyclicalValueDefs
+            Left sortedValueDefs ->
+                Ok $ NormalisedAST dataDefs functionDefs (reverse sortedValueDefs)
+
+type BadDefs ev a = ([FunctionDef ev a], [ValueDef ev a])
+
+
+topSortValueDefinitions :: (Show ev, Show a) => [FunctionDef ev a]
+                        -> [ValueDef ev a]
+                        -> Either [ValueDef ev a] (BadDefs ev a)
+topSortValueDefinitions functionDefs valueDefs =
+    let (depGraph, vertexToNode, _) = dependencyGraph functionDefs valueDefs
+        sccs = map flatten $ scc depGraph
+        cycles = [vertexToNode <$> cyc | cyc <- sccs, cyc `longerThan` 1]
+        forbiddenCycles = filter (any isRight) cycles
+    in
+        case forbiddenCycles of
+            [] -> Left $ rights (vertexToNode <$> topSort depGraph)
+            (badDefs:_) -> Right (lefts badDefs, rights badDefs)
+
+dependencyGraph :: (Show a, Show ev) => [FunctionDef ev a]
+                -> [ValueDef ev a]
+                -> (Graph, Vertex -> Either (FunctionDef ev a) (ValueDef ev a), Id -> Maybe Vertex)
+dependencyGraph functionDefs valueDefs =
+    let graph = graphFromEdges $ (map functionNode functionDefs) ++ (map valueNode valueDefs)
+                where functionNode def = (Left def,  definitionIdentifier def, edges def)
+                      valueNode    def = (Right def, definitionIdentifier def, edges def)
+                      edges        def = Set.toList $ definitionDependencies definitionIds def
+                      definitionIds    = allDefinitionIds functionDefs valueDefs
+    in let (g, vertexToNode, keyToVertex) = graph
+       in (g, (\(~(n, _, _)) -> n) . vertexToNode, keyToVertex)
+
+definitionDependencies :: (Definition d (Expr ev a)) => [Id] -> d -> Set Id
+definitionDependencies defIds def =
+    referencedDefinitions (definitionBody def) defIds
+
+referencedDefinitions :: Expr ev a -> [Id] -> Set Id
+referencedDefinitions ex defIds =
+    Set.filter (`elem` defIds) $ bicata exprDeps valueDeps ex
+    where valueDeps (VarF _ ident) = Set.singleton ident
+          valueDeps other = bifold other
+          exprDeps other = bifold other
+
+longerThan :: [a] -> Int -> Bool
+longerThan list count = (length list) > count
+
+allDefinitionIds :: [FunctionDef ev a] -> [ValueDef ev a] -> [Id]
+allDefinitionIds functionDefs valueDefs =
+    (map definitionIdentifier functionDefs) ++ (map definitionIdentifier valueDefs)
+
+isRecursiveValue :: ValueDef ev a -> Bool
+isRecursiveValue def =
+    not $ Set.null $ definitionDependencies [definitionIdentifier def] def
---
+++ compiler/tests/hspec/Language/Granule/Codegen/TopsortDefinitionsSpec.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE ImplicitParams #-}
+module Language.Granule.Codegen.TopsortDefinitionsSpec where
+
+import Test.Hspec hiding (Spec)
+import qualified Test.Hspec as Test
+import Test.QuickCheck
+import Language.Granule.Syntax.Def
+import Language.Granule.Syntax.Expr
+import Language.Granule.Syntax.Type ((.->))
+import Language.Granule.Utils
+
+import Language.Granule.Codegen.NormalisedDef
+import Language.Granule.Codegen.TopsortDefinitions
+import Language.Granule.Codegen.BuildAST
+
+import Debug.Trace
+
+spec :: Test.Spec
+spec = do
+  describe "topsorting definitions" $ do
+    let ?globals = defaultGlobals
+    it "identifies trivially recursive values" $ do
+        let recursiveValueDef = (defval "x"
+                                    ((val (var "x" int)) `plus` (val (var "add" int)))
+                                    (tts $ int))
+        let expectedResult = RecursiveValues [recursiveValueDef]
+        let actualResult = topologicalSortDefinitions (NormalisedAST [] [] [recursiveValueDef])
+        actualResult `shouldBe` expectedResult
+    it "identifies mutually recursive values" $ do
+        let f = defval "f"
+                    ((val (var "g" int)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let g = defval "g"
+                    ((val (var "f" int)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let h = defval "h"
+                    ((val (lit 11)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let expectedResult = InitializationCycle [] [f, g]
+        let actualResult = topologicalSortDefinitions (NormalisedAST [] [] [f, g, h])
+        actualResult `shouldBe` expectedResult
+    it "identifies more complex mutually recursive values" $ do
+        let f = defval "f"
+                    ((val (var "g" int)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let g = defval "g"
+                    ((val (var "h" int)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let h = defval "h"
+                    ((val (var "f" int)) `plus` (val (lit 10)))
+                    (tts $ int)
+        let expectedResult = InitializationCycle [] [f, g, h]
+        let actualResult = topologicalSortDefinitions (NormalisedAST [] [] [f, g, h])
+        actualResult `shouldBe` expectedResult
+    it "identified initialization cycle in first class function" $ do
+        let f = defun "f" [arg "x" (int .-> int)]
+                    (val (var "x" (int .-> int)))
+                    (tts $ (int .-> int) .-> (int .-> int))
+        let y = defval "y"
+                    (app
+                        (val (var "f" ((int .-> int) .-> (int .-> int))))
+                        (lambdaexp (arg "x" int) (int .-> int)
+                            (app (val (var "n" (int .-> int)))
+                                 (val (var "x" int)))))
+                    (tts $ int .-> int)
+        let n = defun "n" [arg "g" int]
+                    (app (val (var "y" (int .-> int)))
+                         (val (var "g" int)))
+                    (tts $ int .-> int)
+        let main = defval "main"
+                       (app (val (var "y" (int .-> int)))
+                            (val (lit 10)))
+                       (tts $ int)
+
+        let expectedResult = InitializationCycle [n] [y]
+        let actualResult = topologicalSortDefinitions (NormalisedAST [] [f, n] [y, main])
+        actualResult `shouldBe` expectedResult
+    it "accepts trivially valid ast" $ do
+        let valid = (NormalisedAST [] [] [defval "f" (val (lit 10)) (tts $ int)])
+        let actualResult = topologicalSortDefinitions valid
+        actualResult `shouldBe` (Ok valid)
+    it "sorts valid ast containing values only" $ do
+        let f = defval "f"
+                    ((val (var "g" int)) `plus` (val (var "h" int)))
+                    (tts $ int)
+        let g = defval "g"
+                    ((val (var "i" int)) `plus` (val (var "j" int)))
+                    (tts $ int)
+        let h = defval "h" (val (lit 10)) (tts $ int)
+        let i = defval "i" (val (lit 11)) (tts $ int)
+        let j = defval "j" (val (lit 12)) (tts $ int)
+
+        let unsorted = (NormalisedAST [] [] [f, g, h, i, j])
+        let sorted = (NormalisedAST [] [] [i, j, g, h, f])
+        let actualResult = topologicalSortDefinitions unsorted
+        let expectedResult = Ok sorted
+        actualResult `shouldBe` expectedResult
