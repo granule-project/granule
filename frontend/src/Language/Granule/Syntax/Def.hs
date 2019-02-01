602b6a98b0b8463db393baef8bdf939c7af19407
Ben Moon <software@guiltydolphin.com>
2019-02-01 19:13:00 +0000

    Add Freshenable instance for IFace

--- frontend/src/Language/Granule/Syntax/Def.hs
+++ frontend/src/Language/Granule/Syntax/Def.hs
@@ -123,8 +123,10 @@ instance FirstParameter IFaceDat Span
 -- | Fresh a whole AST
 freshenAST :: AST v a -> AST v a
 freshenAST (AST dds defs ifaces insts) =
-  AST dds' defs' ifaces insts
-    where (dds', defs') = (map runFreshener dds, map runFreshener defs)
+  AST dds' defs' ifaces' insts
+    where dds' = map runFreshener dds
+          defs' = map runFreshener defs
+          ifaces' = map runFreshener ifaces
 
 instance Monad m => Freshenable m DataDecl where
   freshen (DataDecl s v tyVars kind ds) = do
@@ -140,6 +142,19 @@ instance Monad m => Freshenable m DataConstr where
   freshen (DataConstrNonIndexed sp v ts) = do
     ts <- mapM freshen ts
     return $ DataConstrNonIndexed sp v ts
+
+instance Monad m => Freshenable m IFace where
+  freshen (IFace sp iname constrs kind pname itys) = do
+    pname' <- freshIdentifierBase Type pname
+    constrs' <- mapM freshen constrs
+    itys' <- mapM freshen itys
+    kind' <- freshen kind
+    return $ IFace sp iname constrs' kind' pname' itys'
+
+instance Monad m => Freshenable m IFaceTy where
+  freshen (IFaceTy sp name tys) = do
+    tys' <- freshen tys
+    return $ IFaceTy sp name tys'
 
 instance Monad m => Freshenable m (Equation v a) where
   freshen (Equation s a ps e) = do
