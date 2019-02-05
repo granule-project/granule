739fd14d43f0efea107fb4123e7c18622c60cf29
Ben Moon <software@guiltydolphin.com>
2019-02-05 20:05:38 +0000

    Fix case of unbound instance parameters

--- frontend/src/Language/Granule/Checker/Checker.hs
+++ frontend/src/Language/Granule/Checker/Checker.hs
@@ -178,7 +178,7 @@ checkIFaceTys (IFace sp iname _ kindAnn pname tys) = do
 checkInstHead :: (?globals :: Globals) => Instance v a -> MaybeT Checker ()
 checkInstHead (Instance sp iname constrs idt _) = do
   checkIFaceExists sp iname
-  mapM_ (kindCheckConstr sp []) constrs
+  mapM_ (kindCheckConstr sp [(v, KType) | v <- freeVars idty]) constrs
   checkInstTy iname idt
 
 checkInstTy :: (?globals :: Globals) => Id -> IFaceDat -> MaybeT Checker ()
