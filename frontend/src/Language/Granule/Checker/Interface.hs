e697f9c241e88ed80865ad1e6dc96de31bf86eb8
Ben Moon <software@guiltydolphin.com>
2019-02-17 15:25:26 +0000

    Move some interface operations into new file
    - somehow I forgot to commit this file a while back...

---
+++ frontend/src/Language/Granule/Checker/Interface.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE ImplicitParams #-}
+
+module Language.Granule.Checker.Interface
+  ( getKindRequired
+  , getInterfaceMembers
+  , getInterfaceParameter
+  , getInterfaceSig
+  , getInterfaceKind
+  ) where
+
+
+import Control.Monad (join)
+import Control.Monad.Trans.Maybe (MaybeT)
+
+import Language.Granule.Syntax.Identifiers (Id)
+import Language.Granule.Syntax.Span (Span)
+import Language.Granule.Syntax.Type
+
+import Language.Granule.Context (Ctxt)
+import Language.Granule.Utils (Globals)
+
+import Language.Granule.Checker.Monad
+
+
+ifaceCtxtNames :: IFaceCtxt -> [Id]
+ifaceCtxtNames = map fst . ifaceSigs
+
+
+getInterface :: Id -> MaybeT Checker (Maybe IFaceCtxt)
+getInterface = lookupContext ifaceContext
+
+
+getInterfaceParameter :: Id -> MaybeT Checker (Maybe Id)
+getInterfaceParameter = fmap (fmap ifaceParam) . getInterface
+
+
+getInterfaceKind :: Id -> MaybeT Checker (Maybe Kind)
+getInterfaceKind = fmap (fmap $ ifaceParamKind) . getInterface
+
+
+getInterfaceSigs :: Id -> MaybeT Checker (Maybe (Ctxt TypeScheme))
+getInterfaceSigs = fmap (fmap ifaceSigs) . getInterface
+
+
+getInterfaceMembers :: Id -> MaybeT Checker (Maybe [Id])
+getInterfaceMembers = fmap (fmap ifaceCtxtNames) . getInterface
+
+
+getInterfaceSig :: Id -> Id -> MaybeT Checker (Maybe TypeScheme)
+getInterfaceSig iname name = fmap join $ fmap (fmap $ lookup name) (getInterfaceSigs iname)
+
+
+-- | Retrieve a kind from the type constructor scope
+getKindRequired :: (?globals :: Globals) => Span -> Id -> MaybeT Checker Kind
+getKindRequired sp name = do
+  ikind <- getInterfaceKind name
+  case ikind of
+    Just k -> pure (KFun k (KConstraint Interface))
+    Nothing -> fmap fst $ requireInScope (typeConstructors, "Type constructor or interface") sp name
