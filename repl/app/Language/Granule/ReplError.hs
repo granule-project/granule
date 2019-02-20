3b588c3a0f67107ef202bef4b4f8ade827edcb02
vilem <v.liepelt@kent.ac.uk>
2019-02-20 00:48:19 +0000

    Fix REPL

--- repl/app/Language/Granule/ReplError.hs
+++ repl/app/Language/Granule/ReplError.hs
@@ -1,33 +1,37 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImplicitParams #-}
+
 module Language.Granule.ReplError where
 
 import Control.Monad.Except()
 import Control.Exception (SomeException)
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty)
+import Data.Foldable (toList)
 
+import Language.Granule.Checker.Monad (CheckerError)
+import Language.Granule.Utils (formatError)
 
 data ReplError = FilePathError String
                | TermInContext String
                | OtherError
-               | OtherError'
+               | TypeCheckerError (NonEmpty CheckerError)
                -- TypeCheckError and ParserError record the filepath queue
                -- so that reloading across files can be done even if we
                -- fail to load a file the first time
-               | TypeCheckError String [FilePath] -- FilePath queue
                | ParseError SomeException [FilePath] -- FilePath queue
                | TermNotInContext String
                | EvalError SomeException
 
 remembersFiles :: ReplError -> Maybe [FilePath]
 remembersFiles (ParseError _ f) = Just f
-remembersFiles (TypeCheckError _ f) = Just f
 remembersFiles _ = Nothing
 
 instance Show ReplError where
   show (FilePathError pth)    = "The file path "<>pth<>" does not exist."
   show (TermInContext trm)    = "The term \""<>trm<>"\" is already in context"
-  show (TypeCheckError pth _)   = "Error type checking "<>pth
   show (ParseError e _)         = show e
   show (TermNotInContext trm) = "The term \""<>trm<>"\" is not in the context"
   show (EvalError e)          = show e
   show OtherError             = "Error"
-  show OtherError'            = ""
+  show (TypeCheckerError err) = let ?globals = mempty in intercalate "\n\n" . map formatError . toList $ err
