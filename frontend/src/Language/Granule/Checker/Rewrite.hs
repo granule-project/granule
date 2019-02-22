7a2571ec16a0aa0fcc341e04af8d098eaae837e5
Ben Moon <software@guiltydolphin.com>
2019-02-22 15:10:05 +0000

    Add module for rewriter

---
+++ frontend/src/Language/Granule/Checker/Rewrite.hs
@@ -0,0 +1,9 @@
+{- |
+Module      :  Language.Granule.Checker.Rewrite
+Description :  Rewrite a type-annotated AST to an intermediate representation
+
+Rewrites a type-annotated AST to an intermediate representation
+without interfaces.
+-}
+
+module Language.Granule.Checker.Rewrite () where
