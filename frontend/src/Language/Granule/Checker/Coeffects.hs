ddfc37a67ffd5c4fcae8659133360a1997bc441a
Dominic Orchard <dom.orchard@gmail.com>
2019-02-28 14:33:09 +0000

    make flatten reflect the paper definition

--- frontend/src/Language/Granule/Checker/Coeffects.hs
+++ frontend/src/Language/Granule/Checker/Coeffects.hs
@@ -15,15 +15,21 @@ import Language.Granule.Syntax.Type
 flattenable :: Type -> Type -> Maybe ((Coeffect -> Coeffect -> Coeffect), Type)
 flattenable t1 t2
  | t1 == t2 = case t1 of
+     t1 | t1 == extendedNat -> Just (CTimes, t1)
+
      TyCon (internalName -> "Nat")   -> Just (CTimes, t1)
      TyCon (internalName -> "Level") -> Just (CJoin, t1)
 
-     TyApp (TyCon (internalName -> "Interval")) t ->  flattenable t t
-     TyApp (TyCon (internalName -> "Ext")) t ->  flattenable t t
      -- TODO
+     TyApp (TyCon (internalName -> "Interval")) t ->  flattenable t t
 
      _ -> Nothing
- | otherwise = Just (CProduct, TyCon (mkId "×") .@ t1 .@ t2)
+ | otherwise =
+     case (t1, t2) of
+        (t1, TyCon (internalName -> "Nat")) | t1 == extendedNat -> Just (CTimes, t1)
+        (TyCon (internalName -> "Nat"), t2) | t2 == extendedNat -> Just (CTimes, t2)
+
+        _ -> Just (CProduct, TyCon (mkId "×") .@ t1 .@ t2)
 
 -- | Multiply an context by a coeffect
 --   (Derelict and promote all variables which are not discharged and are in th
