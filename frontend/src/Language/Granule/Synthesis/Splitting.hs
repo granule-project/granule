ffb2b8203e69b4a6fc3c67e15a16c35418279af5
decb <decb@users.noreply.github.com>
2020-04-08 18:38:47 +0100

    Recursively split under boxes

--- frontend/src/Language/Granule/Synthesis/Splitting.hs
+++ frontend/src/Language/Granule/Synthesis/Splitting.hs
@@ -38,18 +38,69 @@ generateCases span constructors ctxt toSplit = do
   let splitCtxt = relevantSubCtxt toSplit ctxt
   let noSplitCtxt = deleteVars ctxt toSplit
 
-  -- Determines whether an assumption should be treated as linear.
-  let isBoxed (_, a) =
-        case a of
-          Linear (Box _ _) -> True
-          _                -> False
-  let (boxed, notBoxed) = partition isBoxed splitCtxt
+  -- Split the variables we want to.
+  splitPatterns <- splitAll span constructors splitCtxt
+  -- Convert the variables we don't wish to split to trivial patterns.
+  let noSplitPatterns =
+        map (buildVariablePatterns span) (getCtxtIds noSplitCtxt)
 
+  let allPatterns = splitPatterns <> noSplitPatterns
+
+  -- Arrange patterns into the same ordering as the context.
+  let orderedPatterns =
+        map (\(id, _) -> (id, fromJust $ lookup id allPatterns)) ctxt
+
+  -- Convert the patterns into cases (i.e. Cartesian product across patterns).
+  let cases = combineCases orderedPatterns
+
+  -- The Nothing case should be unreachable, as this function is only ever
+  -- called after checkEquation, where equationTy is set.
+  st <- get
+  case equationTy st of
+    Nothing -> return ([], [])
+    Just eqTy -> do
+      -- Filter the patterns if they are impossible.
+      validPatterns <- filterM (caseFilter span eqTy) (snd cases)
+      return (fst cases, validPatterns)
+
+-- Splits all variables in a given context into a list of patterns.
+splitAll ::
+     Span
+  -> Ctxt (Ctxt (TypeScheme, Substitution))
+  -> Ctxt Assumption
+  -> Checker (Ctxt [Pattern ()])
+splitAll span constructors ctxt = do
+  -- Partition the variables we will split by whether they are boxed or not.
+  let (boxed, notBoxed) = partition (isBoxed . snd) ctxt
+
+  -- Splits those variables not in boxes, where possible.
+  (linearPatterns, unsplittable) <- splitNotBoxed span constructors notBoxed
+
+  -- Convert the unsplittables into plain variable patterns.
+  let variablePatterns =
+        map (buildVariablePatterns span) unsplittable
+
+  -- Convert the boxed types into boxed patterns, which may be split recursively
+  -- until we reach a non-box.
+  boxPatterns <- splitBoxed span constructors boxed
+
+  let allPatterns = linearPatterns <> boxPatterns <> variablePatterns
+  return allPatterns
+
+-- Performs case splits on a series of variables which are *not* boxes. Returns
+-- a tuple of patterns and unsplittable variable IDs.
+splitNotBoxed ::
+     Span
+  -> Ctxt (Ctxt (TypeScheme, Substitution))
+  -> Ctxt Assumption
+  -> Checker (Ctxt [Pattern ()], [Id])
+splitNotBoxed _ _ [] = pure ([], [])
+splitNotBoxed span constructors ctxt = do
   -- Spits linear assumptions into splittable/not-splittable. Where splittable
   -- means that it is a data constructor at the highest level (note we filtered
   -- out box patterns in the previous step).
   let (splittable', unsplittable') =
-        partition (isJust . snd) $ map (second getAssumConstr) notBoxed
+        partition (isJust . snd) $ map (second getAssumConstr) ctxt
   let splittable = map (second fromJust) splittable'
   let unsplittable = getCtxtIds unsplittable'
 
@@ -59,31 +110,33 @@ generateCases span constructors ctxt toSplit = do
   linearPatterns <-
     mapM (uncurry (buildConstructorPatterns span)) relConstructors
 
-  -- Convert the unsplittables into plain variable patterns.
-  let variablePatterns =
-        map (buildVariablePatterns span) (unsplittable ++ getCtxtIds noSplitCtxt)
+  return (linearPatterns, unsplittable)
 
-  -- Convert the discharged types into boxed patterns.
-  let boxPatterns = map (buildBoxPattern span . fst) boxed
+-- Performs case splits on a series of variables which *are* boxes. Returns a
+-- context linking each variable to its corresponding to patterns. Note that
+-- we recursively split boxes until we reach something that isn't a box, then
+-- split that once.
+splitBoxed ::
+     Span
+  -> Ctxt (Ctxt (TypeScheme, Substitution))
+  -> Ctxt Assumption
+  -> Checker (Ctxt [Pattern ()])
+splitBoxed _ _ [] = pure []
+splitBoxed span constructors ctxt = do
+  -- Gets the type inside the box.
+  let extractSubtype t =
+        case t of
+          Linear (Box _ t') -> Linear t'
+          Discharged (Box _ t') cf -> Discharged t' cf
+          t -> t -- Note: this case should never be reached.
+  let subtypeCtxt = map (second extractSubtype) ctxt
 
-  let allPatterns = linearPatterns <> boxPatterns <> variablePatterns
+  -- Recursively split all variables, using their subtype.
+  allPatterns <- splitAll span constructors subtypeCtxt
 
-  -- Order patterns into the same ordering as the context.
-  let orderedPatterns =
-        map (\(id, _) -> (id, fromJust $ lookup id allPatterns)) ctxt
-
-  -- Convert the patterns into cases (i.e. Cartesian product of patterns).
-  let cases = combineCases orderedPatterns
-
-  -- The Nothing case should be unreachable, as this function is only ever
-  -- called after checkEquation where equationTy is set.
-  st <- get
-  case equationTy st of
-    Nothing -> return ([], [])
-    Just eqTy -> do
-      -- Filter the patterns if they are impossible.
-      validPatterns <- filterM (caseFilter span eqTy) (snd cases)
-      return (fst cases, validPatterns)
+  -- We box all of the patterns generated to get the result.
+  let result = map (second (map (buildBoxPattern span))) allPatterns
+  return result
 
 -- Wrapper around validateCase which updates the state when the case is valid.
 caseFilter :: (?globals :: Globals)
@@ -157,20 +210,21 @@ relevantDataConstrs constructors types =
 
 -- Gets a potential constructor identifier on a type constructor, recursively.
 getAssumConstr :: Assumption -> Maybe Id
-getAssumConstr (Discharged t _) = getTypeConstr t
-getAssumConstr (Linear t) = getTypeConstr t
-
-getTypeConstr :: Type -> Maybe Id
-getTypeConstr (FunTy _ t1 _) = Nothing
-getTypeConstr (TyCon id) = Just id
-getTypeConstr (Box _ t) = getTypeConstr t
-getTypeConstr (Diamond t1 _) = getTypeConstr t1
-getTypeConstr (TyApp t1 t2) = getTypeConstr t1
-getTypeConstr (TySig t _) = getTypeConstr t
-getTypeConstr (TyVar _) = Nothing
-getTypeConstr (TyInt _) = Nothing
-getTypeConstr (TyInfix _ _ _) = Nothing
-getTypeConstr (TySet _) = Nothing
+getAssumConstr a =
+  case a of
+    (Discharged t _) -> getTypeConstr t
+    (Linear t) -> getTypeConstr t
+  where
+    getTypeConstr (FunTy _ t1 _) = Nothing
+    getTypeConstr (TyCon id) = Just id
+    getTypeConstr (Box _ t) = getTypeConstr t
+    getTypeConstr (Diamond t1 _) = getTypeConstr t1
+    getTypeConstr (TyApp t1 t2) = getTypeConstr t1
+    getTypeConstr (TySig t _) = getTypeConstr t
+    getTypeConstr (TyVar _) = Nothing
+    getTypeConstr (TyInt _) = Nothing
+    getTypeConstr (TyInfix _ _ _) = Nothing
+    getTypeConstr (TySet _) = Nothing
 
 -- Given a list of data constructors, generates patterns corresponding to them.
 buildConstructorPatterns ::
@@ -203,10 +257,9 @@ buildConstructorPatterns span id constructors = do
 buildVariablePatterns :: Span -> Id -> (Id, [Pattern ()])
 buildVariablePatterns span id = (id, pure $ PVar span () False id)
 
--- Given an identifier, generates a box pattern containing that identifier,
--- paired with the identifier itself.
-buildBoxPattern :: Span -> Id -> (Id, [Pattern ()])
-buildBoxPattern span id = (id, pure $ PBox span () False (PVar span () False id))
+-- Wraps a pattern in a box
+buildBoxPattern :: Span -> Pattern () -> Pattern ()
+buildBoxPattern span = PBox span () False
 
 -- Accumulates identifiers which label function types.
 tsTypeNames :: TypeScheme -> [Maybe Id]
@@ -220,3 +273,9 @@ tsTypeNames (Forall _ _ _ t) = typeNames t
 -- the patterns.
 combineCases :: Ctxt [Pattern ()] -> ([Id], [[Pattern ()]])
 combineCases pats = (map fst pats, mapM snd pats)
+
+-- Determines whether an assumption contains a box type.
+isBoxed :: Assumption -> Bool
+isBoxed (Linear (Box _ _)) = True
+isBoxed (Discharged (Box _ _) _) = True
+isBoxed _ = False
