25fbd4912885f415a2c463fad455052a3a70dd67
dominic <dom.orchard@gmail.com>
2020-06-15 14:08:24 +0100

    optimise appHelper by checking variable types first

--- frontend/src/Language/Granule/Synthesis/Synth.hs
+++ frontend/src/Language/Granule/Synthesis/Synth.hs
@@ -424,42 +424,48 @@ appHelper decls left [] _ _ = none
 
 appHelper decls left (var@(x, a) : right) (sub@Subtractive{}) goalTy@(Forall _ binders constraints _ ) =
   (appHelper decls (var : left) right sub goalTy) `try`
-  let omega = left ++ right in do
-  (canUse, omega', t) <- useVar var omega sub
-  case (canUse, t) of
-    (True, FunTy _ t1 t2) -> do
-        id <- freshIdentifier
-        let (gamma', omega'') = bindToContext (id, Linear t2) omega' [] (isLAsync t2)
-        (e1, delta1, sub1) <- synthesiseInner decls True sub gamma' omega'' goalTy
-        case lookup id delta1 of
-          Nothing -> do
-            -- Check that `id` was used by `e1` (and thus is not in `delta1`)
-            (e2, delta2, sub2) <- synthesiseInner decls True sub delta1 [] (Forall nullSpanNoFile binders constraints t1)
-            subst <- conv $ combineSubstitutions nullSpanNoFile sub1 sub2
-            return (Language.Granule.Syntax.Expr.subst (makeApp x e2 goalTy t) id e1, delta2, subst)
-          _ -> none
-    _ -> none
+  (case getAssumptionType a of
+    (FunTy _ t1 t2) -> do
+      let omega = left ++ right
+      (canUse, omega', t) <- useVar var omega sub
+      if canUse
+        then do
+          id <- freshIdentifier
+          let (gamma', omega'') = bindToContext (id, Linear t2) omega' [] (isLAsync t2)
+          (e1, delta1, sub1) <- synthesiseInner decls True sub gamma' omega'' goalTy
+          case lookup id delta1 of
+            Nothing -> do
+              -- Check that `id` was used by `e1` (and thus is not in `delta1`)
+              (e2, delta2, sub2) <- synthesiseInner decls True sub delta1 [] (Forall nullSpanNoFile binders constraints t1)
+              subst <- conv $ combineSubstitutions nullSpanNoFile sub1 sub2
+              return (Language.Granule.Syntax.Expr.subst (makeApp x e2 goalTy t) id e1, delta2, subst)
+            _ -> none
+        else none
+    _ -> none)
+
 appHelper decls left (var@(x, a) : right) (add@(Additive mode)) goalTy@(Forall _ binders constraints _ ) =
   (appHelper decls (var : left) right add goalTy) `try`
-  let omega = (var:left) ++ right in do
-    (canUse, omega', t) <- useVar var omega add
-    case (canUse, t) of
-      (True, FunTy _ t1 t2) -> do
-        id <- freshIdentifier
-        gamma1 <- ctxtSubtract omega omega'
-        let (gamma1', omega'') = bindToContext (id, Linear t2) gamma1 [] (isLAsync t2)
-        (e1, delta1, sub1) <- synthesiseInner decls True add gamma1' omega'' goalTy
-        case lookupAndCutout id delta1 of
-          Just (delta1', Linear _) -> do
-            gamma2 <- ctxtSubtract (gamma1' ++ omega'') delta1'
-            (e2, delta2, sub2) <- synthesiseInner decls True add gamma2 [] (Forall nullSpanNoFile binders constraints t1)
-            delta3 <- maybeToSynthesiser $ ctxtAdd omega' delta1'
-            delta4 <- maybeToSynthesiser $ ctxtAdd delta2 delta3
-            subst <- conv $ combineSubstitutions nullSpan sub1 sub2
-            return (Language.Granule.Syntax.Expr.subst (makeApp x e2 goalTy t) id e1, delta4, subst)
-          _ -> none
-      _ -> none
-
+  (case getAssumptionType a of
+    (FunTy _ t1 t2) -> do
+      let omega = (var:left) ++ right
+      (canUse, omega', t) <- useVar var omega add
+      if canUse
+        then do
+          id <- freshIdentifier
+          gamma1 <- ctxtSubtract omega omega'
+          let (gamma1', omega'') = bindToContext (id, Linear t2) gamma1 [] (isLAsync t2)
+          (e1, delta1, sub1) <- synthesiseInner decls True add gamma1' omega'' goalTy
+          case lookupAndCutout id delta1 of
+            Just (delta1', Linear _) -> do
+              gamma2 <- ctxtSubtract (gamma1' ++ omega'') delta1'
+              (e2, delta2, sub2) <- synthesiseInner decls True add gamma2 [] (Forall nullSpanNoFile binders constraints t1)
+              delta3 <- maybeToSynthesiser $ ctxtAdd omega' delta1'
+              delta4 <- maybeToSynthesiser $ ctxtAdd delta2 delta3
+              subst <- conv $ combineSubstitutions nullSpan sub1 sub2
+              return (Language.Granule.Syntax.Expr.subst (makeApp x e2 goalTy t) id e1, delta4, subst)
+            _ -> none
+        else none
+    _ -> none)
 
 boxHelper :: (?globals :: Globals)
   => Ctxt DataDecl
