aba1259669c0a73d2ce5c8c5241bac602aaadeb4
dominic <dom.orchard@gmail.com>
2020-10-14 15:27:01 +0100

    primitive

--- frontend/src/Language/Granule/Checker/Primitives.hs
+++ frontend/src/Language/Granule/Checker/Primitives.hs
@@ -40,7 +40,7 @@ kindConstructor =
 typeConstructors :: [(Id, (Type, [Id], Bool))] -- TODO Cardinality is not a good term
 typeConstructors =
     [ (mkId "->",     (funTy (Type 0) (funTy (Type 0) (Type 0)), [], False))
-    , (mkId "×",      (funTy kcoeffect (funTy kcoeffect kcoeffect), [mkId ","], False))
+    , (mkId "×",      (funTy kcoeffect (funTy kcoeffect kcoeffect), [mkId ", "], False))
     , (mkId "Int",    (Type 0, [], False))
     , (mkId "Float",  (Type 0, [], False))
     , (mkId "Char",   (Type 0, [], False))
@@ -293,7 +293,6 @@ grecv = BUILTIN
 
 gclose : Chan End -> () <Session>
 gclose = BUILTIN
-
 
 -- trace : String -> () <>
 -- trace = BUILTIN
