15a3c6b693242bd424a4ddab02f5a9d8906a2014
Dominic Orchard <dom.orchard@gmail.com>
2022-12-19 16:35:51 +0000

    pretty print function which needs to global config;

--- frontend/src/Language/Granule/Syntax/Pretty.hs
+++ frontend/src/Language/Granule/Syntax/Pretty.hs
@@ -23,6 +23,11 @@ import Language.Granule.Syntax.Helpers
 import Language.Granule.Syntax.Identifiers
 import Language.Granule.Utils
 
+-- Version of pretty that assumes the default Globals so as not to
+-- propagate globals information around
+prettyTrace :: Pretty t => t -> String
+prettyTrace = let ?globals = mempty in pretty
+
 prettyDebug :: (?globals :: Globals) => Pretty t => t -> String
 prettyDebug x =
   let ?globals = ?globals { globalsDebugging = Just True }
