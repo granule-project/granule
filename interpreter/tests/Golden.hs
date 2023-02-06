500c9d0ba4e9479c384355fd537af4d6fe7afa11
Dominic Orchard <dom.orchard@gmail.com>
2023-02-06 17:33:51 +0000

    WIP on allowing exclusion of certain tests"

--- interpreter/tests/Golden.hs
+++ interpreter/tests/Golden.hs
@@ -2,52 +2,112 @@ import Control.Exception (catch, throwIO)
 import Control.Monad (unless)
 import Data.Algorithm.Diff (getGroupedDiff)
 import Data.Algorithm.DiffOutput (ppDiff)
-import Data.List (sort)
+import Data.List (sort, isInfixOf)
 import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsFile)
 import qualified Test.Tasty.Golden as G
 import Test.Tasty.Golden.Advanced (goldenTest)
 import System.Directory (renameFile, setCurrentDirectory)
 import System.Exit (ExitCode)
-import System.FilePath (dropExtension)
+import System.FilePath (dropExtension, pathSeparator)
 import qualified System.IO.Strict as Strict (readFile)
+import System.Environment
 
 import Language.Granule.Interpreter (InterpreterResult(..), InterpreterError(..))
 import qualified Language.Granule.Interpreter as Interpreter
 import Language.Granule.Syntax.Pretty (pretty)
 import Language.Granule.Utils (Globals (..), formatError)
 
+data Config = IncludeAll Config | Include String Config | Exclude String Config | Nil
+  deriving Show
+
+{-
+-- e.g., aux "-p simple -p polymorphism -e simple"
+--    == IncludeAll (Include "simple" (Include "polymorphism" (Exclude "simple" Nil)))
+parseArgsToConfig :: [String] -> Either String Config
+-- Otherwise we need to parse the inclusions/exclusions
+parseArgsToConfig config = (postProcess False Nil) <$> (aux config)
+  where
+    -- The list of arguments has includes therefore we will start with no files and add
+    postProcess True  res Nil = res
+    -- The list of arguments has no includes, therefore include all
+    postProcess False res Nil = IncludeAll res
+    -- Induct
+    postProcess hasInclude res (Include p as) = postProcess True (Include p res) as
+    postProcess hasInclude res (Exclude p as) = postProcess hasInclude (Exclude p res) as
+    postProcess hasInclude res (IncludeAll c) = postProcess hasInclude (IncludeAll res) c
+
+    -- Any argument starting with `--` is not one we are recognising here
+    aux ("--all":zs)     = aux zs >>= return . IncludeAll
+    aux (('-':'-':_):zs) = aux zs
+    aux ("-a":zs) =
+      (aux zs) >>= (return . IncludeAll)
+    aux (('-':mode):pattern:zs) =
+      case mode of
+        "p" -> (aux zs) >>= (return . Include pattern)
+        "e" -> (aux zs) >>= (return . Exclude pattern)
+        _   -> Left $ "Unknown mode: " <> mode
+    aux (_:xs) = aux xs
+    aux []     = return Nil
+-}
+
 main :: IO ()
 main = do
-  -- go into project root
-  setCurrentDirectory "../"
-  negative <- goldenTestsNegative
-  positive <- goldenTestsPositive
-  rewrite <- goldenTestsRewrite
-  synthesis <- goldenTestsSynthesis
+  -- Get patterns from test arguments
+  -- e.g., stack test --ta '-p positive'
+  args <- getArgs
+  let configE = Right (IncludeAll Nil) -- parseArgsToConfig args
+  case configE of
+    Left error -> do
+      putStrLn $ "Error in test arguments: " <> error
+    Right config -> do
+      -- go into project root
+      setCurrentDirectory "../"
+      negative  <- goldenTestsNegative  config
+      positive  <- goldenTestsPositive  config 
+      rewrite   <- goldenTestsRewrite   config
+      synthesis <- goldenTestsSynthesis config
 
-  catch
-    (defaultMain $ testGroup "Golden tests" [negative, positive, rewrite, synthesis])
-    (\(e :: ExitCode) -> do
-      -- Move all of the backup files back to their original place.
-      backupFiles <- findByExtension [".bak"]  "frontend/tests/cases/rewrite"
-      _ <- mapM_ (\backup -> renameFile backup (dropExtension backup)) backupFiles
-      -- and for synthesis
-      backupFiles <- findByExtension [".bak"]  "frontend/tests/cases/synthesis"
-      _ <- mapM_ (\backup -> renameFile backup (dropExtension backup)) backupFiles
-      throwIO e
-    )
+      catch
+        (defaultMain $ testGroup "Golden tests" [negative, positive, rewrite, synthesis])
+        (\(e :: ExitCode) -> do
+          -- Move all of the backup files back to their original place.
+          backupFiles <- findByExtension config [".bak"]  "frontend/tests/cases/rewrite"
+          _ <- mapM_ (\backup -> renameFile backup (dropExtension backup)) backupFiles
+          -- and for synthesis
+          backupFiles <- findByExtension config [".bak"]  "frontend/tests/cases/synthesis"
+          _ <- mapM_ (\backup -> renameFile backup (dropExtension backup)) backupFiles
+          throwIO e
+        )
 
-findByExtension :: [FilePath] -> FilePath -> IO [FilePath]
-findByExtension exs path = G.findByExtension exs path >>= (return . sort)
+-- Applies a configuration to list of filepaths
+applyConfig :: Config -> [FilePath] -> [FilePath]
+applyConfig cfgs files = aux cfgs []
+  where
+    aux (IncludeAll cfg) _soFar =
+      aux cfg files
 
-goldenTestsNegative :: IO TestTree
-goldenTestsNegative = do
+    -- Add from the files list those from a directory matching this pattern
+    aux (Include pat cfg) soFar =
+      aux cfg (soFar ++ filter (\file -> (pat ++ [pathSeparator]) `isInfixOf` file) files)
+
+    -- Remove from the soFar list those from a directory matching this pattern
+    aux (Exclude pat cfg) soFar =
+      aux cfg (filter (\file -> not $ (pat ++ [pathSeparator]) `isInfixOf` file) soFar)
+
+    aux Nil soFar = soFar
+
+
+findByExtension :: Config -> [FilePath] -> FilePath -> IO [FilePath]
+findByExtension config exs path = G.findByExtension exs path >>= (return . sort . applyConfig config)
+
+goldenTestsNegative :: Config -> IO TestTree
+goldenTestsNegative config = do
   -- get example files, but discard the excluded ones
-  files <- findByExtension granuleFileExtensions "frontend/tests/cases/negative"
+  files <- findByExtension config granuleFileExtensions "frontend/tests/cases/negative"
 
   -- ensure we don't have spurious output files without associated tests
-  outfiles <- findByExtension [".output"] "frontend/tests/cases/negative"
+  outfiles <- findByExtension config [".output"] "frontend/tests/cases/negative"
   failOnOrphanOutfiles files outfiles
 
   return $ testGroup
@@ -61,17 +121,17 @@ goldenTestsNegative = do
         Right x -> error $ "Negative test passed!\n" <> show x
 
 
-goldenTestsPositive :: IO TestTree
-goldenTestsPositive = do
+goldenTestsPositive :: Config -> IO TestTree
+goldenTestsPositive config = do
   -- get example files, but discard the excluded ones
-  exampleFiles  <- findByExtension granuleFileExtensions "examples"
-  stdLibFiles   <- findByExtension granuleFileExtensions "StdLib"
-  positiveFiles <- findByExtension granuleFileExtensions "frontend/tests/cases/positive"
+  exampleFiles  <- findByExtension config granuleFileExtensions "examples"
+  stdLibFiles   <- findByExtension config granuleFileExtensions "StdLib"
+  positiveFiles <- findByExtension config granuleFileExtensions "frontend/tests/cases/positive"
   let files = exampleFiles <> stdLibFiles <> positiveFiles
 
   -- ensure we don't have spurious output files without associated tests
-  exampleOutfiles  <- findByExtension [".output"] "examples"
-  positiveOutfiles <- findByExtension [".output"] "frontend/tests/cases/positive"
+  exampleOutfiles  <- findByExtension config [".output"] "examples"
+  positiveOutfiles <- findByExtension config [".output"] "frontend/tests/cases/positive"
   let outfiles = exampleOutfiles <> positiveOutfiles
   failOnOrphanOutfiles files outfiles
 
@@ -86,15 +146,15 @@ goldenTestsPositive = do
         Left err -> error $ formatError err
         Right NoEval -> mempty
 
-goldenTestsRewrite :: IO TestTree
-goldenTestsRewrite = do
+goldenTestsRewrite :: Config -> IO TestTree
+goldenTestsRewrite config = do
   let dir = "frontend/tests/cases/rewrite"
 
   -- get example files, but discard the excluded ones
-  files <- findByExtension granuleFileExtensions dir
+  files <- findByExtension config granuleFileExtensions dir
 
   -- ensure we don't have spurious output files without associated tests
-  outfiles <- findByExtension [".output"] dir
+  outfiles <- findByExtension config [".output"] dir
   failOnOrphanOutfiles files outfiles
 
   return $ testGroup
@@ -116,20 +176,20 @@ goldenTestsRewrite = do
       _ <- Interpreter.run (mempty { Interpreter.grKeepBackup = Just True }) src
       return ()
 
-goldenTestsSynthesis :: IO TestTree
-goldenTestsSynthesis = do
+goldenTestsSynthesis :: Config -> IO TestTree
+goldenTestsSynthesis config = do
   let dir = "frontend/tests/cases/synthesis"
 
   -- get example files, but discard the excluded ones
-  files <- findByExtension granuleFileExtensions dir
+  files <- findByExtension config granuleFileExtensions dir
 
   -- ensure we don't have spurious output files without associated tests
-  outfiles <- findByExtension [".output"] dir
+  outfiles <- findByExtension config [".output"] dir
   failOnOrphanOutfiles files outfiles
 
   -- subtractive synthesis check
   let hdir = "frontend/tests/cases/synthesis/hilbert"
-  hfiles <- findByExtension granuleFileExtensions hdir
+  hfiles <- findByExtension config granuleFileExtensions hdir
 
   return $ testGroup
     "Golden synthesis examples"
