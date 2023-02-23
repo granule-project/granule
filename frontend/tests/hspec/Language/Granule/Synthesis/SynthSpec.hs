e0d03f2361d5fb28d30d24edc9141d632e700311
dorchard <dom.orchard@gmail.com>
2023-02-23 17:40:34 +0000

    add initial test for Synth.Common

---
+++ frontend/tests/hspec/Language/Granule/Synthesis/SynthSpec.hs
@@ -0,0 +1,31 @@
+module Language.Granule.Synthesis.SynthSpec where
+
+import Test.Hspec hiding (Spec)
+import qualified Test.Hspec as Test
+
+import Language.Granule.Synthesis.Common
+import Language.Granule.Synthesis.Monad
+import Language.Granule.Syntax.Type
+import Language.Granule.Syntax.Identifiers
+import Language.Granule.Checker.Monad(initState)
+import Language.Granule.Utils
+
+import Control.Monad.State.Strict
+
+spec :: Test.Spec
+spec = let ?globals = mempty :: Globals in do
+  describe "Tests on checkConstructor" $ do
+    it "Monomorphic tests" $ do
+      -- A unit constructor matches a unit goal
+      constr <- testSynthesiser
+                   $ checkConstructor False (Forall ns [] [] (TyCon (mkId "()"))) (TyCon (mkId "()")) []
+      let status = map (fmap (\(status, _, _, _, _) -> status)) constr
+      status `shouldBe` [Just True]
+
+testSynthesiser :: (?globals :: Globals) => Synthesiser a -> IO [Maybe a]
+testSynthesiser m = do
+    (outputs, _) <- runStateT (runSynthesiser 0 m initState) mempty
+    return $ map (convertError . fst) outputs
+  where
+    convertError (Right a) = Just a
+    convertError (Left _)  = Nothing
