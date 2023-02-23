5a8f4d3fe6a3abd4e0f635a997462c6e1c7a104c
dorchard <dom.orchard@gmail.com>
2023-02-23 19:59:44 +0000

    fix Synthesis.Common tests runner

--- frontend/tests/hspec/Language/Granule/Synthesis/SynthSpec.hs
+++ frontend/tests/hspec/Language/Granule/Synthesis/SynthSpec.hs
@@ -1,4 +1,4 @@
-module Language.Granule.Synthesis.SynthSpec where
+module Language.Granule.Synthesis.CommonSpec where
 
 import Test.Hspec hiding (Spec)
 import qualified Test.Hspec as Test
@@ -7,12 +7,15 @@ import Language.Granule.Synthesis.Common
 import Language.Granule.Synthesis.Monad
 import Language.Granule.Syntax.Type
 import Language.Granule.Syntax.Identifiers
-import Language.Granule.Checker.Monad(initState)
+import Language.Granule.Checker.Checker(checkDataCons,checkTyCon)
+import qualified Language.Granule.Checker.Primitives as Primitives
+import Language.Granule.Checker.Monad(initState,runAll)
 import Language.Granule.Utils
 
 import Control.Monad.State.Strict
 
--- To run just these tests do: stack test --test-arguments "-m "Synth""
+-- To run just these tests do:
+--  stack test granule-frontend --test-arguments "-m "Common""
 
 spec :: Test.Spec
 spec = let ?globals = mempty :: Globals in do
@@ -25,9 +28,17 @@ spec = let ?globals = mempty :: Globals in do
       status `shouldBe` [Just True]
 
 testSynthesiser :: (?globals :: Globals) => Synthesiser a -> IO [Maybe a]
-testSynthesiser m = do
-    (outputs, _) <- runStateT (runSynthesiser 0 m initState) mempty
-    return $ map (convertError . fst) outputs
+testSynthesiser synthComputation = do
+    -- Load in the primitive data constructors first before running the computation synthComputation
+    let synthComputation' =
+             (conv (runAll checkTyCon Primitives.dataTypes))
+          >> (conv (runAll checkDataCons Primitives.dataTypes))
+          >> synthComputation
+    (outputs, dat) <- runStateT (runSynthesiser 1 synthComputation' initState) mempty
+    mapM (convertError . fst) outputs
   where
-    convertError (Right a) = Just a
-    convertError (Left _)  = Nothing
+    convertError (Right a) = return $ Just a
+    convertError (Left err) = do
+      -- Print error message if something went badly wrong
+      putStrLn $ show err
+      return $ Nothing
