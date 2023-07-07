0b07bd2b0af68a84f9fe86db24ff476f9cdd24b3
dorchard <dom.orchard@gmail.com>
2023-07-07 23:07:56 +0100

    debugging

--- frontend/src/Language/Granule/Checker/Types.hs
+++ frontend/src/Language/Granule/Checker/Types.hs
@@ -133,7 +133,7 @@ equalTypesRelatedCoeffectsInner s rel t1 t2 _ _ _ | t1 == t2 =
   return (True, [])
 
 equalTypesRelatedCoeffectsInner s rel fTy1@(FunTy _ t1 t2) fTy2@(FunTy _ t1' t2') _ sp mode = do
-  debugM "equalTypesRelatedCoeffectsInner (funTy left)" (pretty sp)
+  debugM "equalTypesRelatedCoeffectsInner (funTy left)" (pretty fTy1 <> " =? " <> pretty fTy2 <> " and " <> pretty sp)
   -- contravariant position (always approximate)
   (eq1, u1) <- equalTypesRelatedCoeffects s ApproximatedBy t1' t1 (flipIndicator sp) mode
    -- covariant position (depends: is not always over approximated)
