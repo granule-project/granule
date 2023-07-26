91b0ad328560019922e148014d17613c78821adc
Ben Orchard <thefirstmuffinman@gmail.com>
2023-07-26 13:23:52 +0100

    Nix flake: add granule stdlib, grepl wrapper

--- flake.nix
+++ flake.nix
@@ -19,6 +19,25 @@
 
       perSystem = { self', pkgs, config, ... }: {
         packages.default = self'.packages.granule-repl;
+
+        # TODO shame I have to create a full derivation for this, I'd like to
+        # just copy files with a name. alas
+        packages.granule-stdlib = pkgs.stdenv.mkDerivation {
+          name = "granule-stdlib";
+          src = ./StdLib;
+          phases = [ "unpackPhase" "installPhase" ];
+          installPhase = ''
+            mkdir -p $out
+            cp $src/* $out
+          '';
+        };
+
+        packages.granule-repl-with-stdlib = pkgs.writeShellScriptBin "grepl" ''
+          ${self'.packages.granule-repl}/bin/grepl \
+            --include-path ${self'.packages.granule-stdlib} \
+            $@
+        '';
+
         #haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
         haskellProjects.default = {
           #basePackages = config.haskellProjects.ghc96.outputs.finalPackages;
@@ -66,13 +85,14 @@
         # for podman, `nix build .#image && ./result | podman load`
         # for some reason, I don't need justStaticExecutables to get a small
         # image here. not sure why but sure!
-        packages.image-repl = pkgs.dockerTools.streamLayeredImage {
+        packages.image-granule-repl = pkgs.dockerTools.streamLayeredImage {
           name = "granule-repl";
           # equivalent to `git rev-parse HEAD`
           # only exists on clean working tree, else set to "dev"
           tag = self.rev or "dev";
           config = {
-            Entrypoint = [ "${pkgs.lib.getExe self'.packages.granule-repl}" ];
+            #Entrypoint = [ "${pkgs.lib.getExe self'.packages.granule-repl}" ];
+            Entrypoint = [ "${self'.packages.granule-repl-with-stdlib}/bin/grepl" ];
           };
           #contents = [ self'.packages.granule-repl ];
           maxLayers = 100; # less than Docker max layers to allow extending
